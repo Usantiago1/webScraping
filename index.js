f9d8a847baf4661bc94ee5dc35e3e5c2fb3eee3e
usantiago1 <usantiago0309@gmail.com>
2022-06-29 21:41:43 -0500

    Extraccion de datos a un excel

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const cheerio = require('cheerio');
 const request = require('request-promise');
+const fs = require('fs-extra');
+
+    const writeStream = fs.createWriteStream('data.csv');
 
    async function init (){
         const $ =  await  request({
             uri: 'http://quotes.toscrape.com/',
             transform: body => cheerio.load(body)
         });
-        const title = $('title');
-        console.log(title.html());
 
-        const heading = $('h1');
-        console.log(heading.text());
+        writeStream.write('Quote|Autor|Tags\n');
+        $('.quote').each((i, el) => {
+            const text = $(el).find('span.text').text().replace(/(^\“|\”$)/g, "");
+            const author = $(el).find('span small.author').text();
+            //const tag = $(el).find('.tags a').hmtl();
+            const tags = [];
+            $(el).find('.tags a.tag').each((i, el) => tags.push($(el).text()));
+            writeStream.write(`${text}|${author}|${tags}\n`);
+
+        })
     }
     init();
